54ceabda9f3b4328d8a3eeb31292bc2732ed3e27
zhuzyu <zhuzyu@yonyou.com>
2021-03-09 21:18:35 +0800

    update

--- src/ksum/sum3.ts
+++ src/ksum/sum3.ts
@@ -11,16 +11,32 @@ module.exports = function sum3(array: Array<number>):Array<Array<number>> {
   let k = 2;
 
   const ret:Array<Array<number>> = [];
-  for ( ; i < format.length - 2; i++) {
-    for (j = i + 1; j < format.length - 1; j++) {
-      for (k = j + 1; k < format.length; k++ ) {
-        const count = format[i] + format[j] + format[k];
-        if (count === 0) {
-          ret.push([format[i], format[j], format[k]]);
-        }
+  // for ( ; i < format.length - 2; i++) {
+  //   for (j = i + 1; j < format.length - 1; j++) {
+  //     for (k = j + 1; k < format.length; k++ ) {
+  //       const count = format[i] + format[j] + format[k];
+  //       if (count === 0) {
+  //         ret.push([format[i], format[j], format[k]]);
+  //       }
+  //     }
+  //   }
+  // }
+
+  for ( ; i < format.length - 2 && format[i] < 0; i++) {
+    for (j = i + 1, k = format.length; j < k; ) {
+      const count = format[i] + format[j] + format[k];
+      if (count === 0) {
+        ret.push([format[i], format[j], format[k]]);
+        // end
+        j = k;
+      } else if (count < 0) {
+        j++;
+      } else {
+        k--;
       }
     }
   }
+
 
   return ret;
 };
