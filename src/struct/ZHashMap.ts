92690b434e6fc9fe2c58084ae5e72d1d3715808e
zhuzyu <zzyy1025>
2021-04-07 18:07:29 +0800

    save

--- src/struct/ZHashMap.ts
+++ src/struct/ZHashMap.ts
@@ -1,56 +1,66 @@
 import {ZLinkedList} from './ZLinkedList';
 
 export class ZHashMap<T> {
-  private head;
-  private length;
+    private readonly head: Array<ZLinkedList<ZHashMapNode<T>>>;
+    private length;
+    private DEFAULT_MAX_HEAD_LENGTH = 100;
 
-  /**
-   *
-   * @param { number } length
-   */
-  constructor(length: number) {
-    this.length = length;
-    this.head = new Array(length);
-    this.head.forEach((head, index, arr)=>{
-      arr[index] = new ZLinkedList<T>();
-    });
-  }
-
-  /**
-   *
-   * @param { number } key
-   * @return { number } hashIndex
-   */
-  hash(key: number) {
-    return key % length;
-  }
-
-  /**
-   *
-   * @param { ZHashMapNode } node
-   */
-  add(node: ZHashMapNode<any>) {
-    const hasNode = this.head[this.hash(node.key)].find(node);
-    if (hasNode) {
-      hasNode;
+    /**
+     *
+     * @param { number } length
+     */
+    constructor(length: number) {
+      this.length = length || this.DEFAULT_MAX_HEAD_LENGTH;
+      this.head = new Array(length);
+      this.head.forEach((head, index, arr) => {
+        arr[index] = new ZLinkedList<ZHashMapNode<T>>();
+      });
     }
-  }
 
-  modify() {
+    /**
+     *
+     * @param { number } key
+     * @return { number } hashIndex
+     */
+    hash(key: number) {
+      return key % length;
+    }
 
-  }
+    /**
+     *
+     * @param { ZHashMapNode } node
+     */
+    add(node: ZHashMapNode<T>) {
+      const set = this.head[this.hash(node.key)];
+      set.add(node);
+    }
 
-  delete(key:number) {
+    modify(key: number, value: T) {
+      const set = this.head[this.hash(key)];
+      for (const entry of set) {
+        if (entry.value.data.key === key) {
+          entry.value.data.value = value;
+        }
+      }
+    }
 
-  }
+
+    find(key:number) {
+      const set = this.head[this.hash(key)];
+    //  for
+    }
+
+    delete(key: number) {
+
+    }
 };
 
 class ZHashMapNode<T> {
-  key;
-  value;
+    key: number;
+    value: T;
 
-  constructor(key: number, value :T) {
-    this.key = key;
-    this.value = value;
-  }
+    constructor(key: number, value: T) {
+      this.key = key;
+      this.value = value;
+    }
 }
--- src/struct/ZSetUnion.ts
+++ src/struct/ZSetUnion.ts
@@ -1,10 +1,18 @@
-// import {ZLinkedList} from '@/struct/ZLinkedList';
-// export class ZSetUnion<T> {
-//     private DEFAULT_MAX_LENGTH = 100;
-//     private set:Array<ZLinkedList<T>>;
-//
-//     constructor(length: number) {
-//       length ||= this.DEFAULT_MAX_LENGTH;
-//       this.set = new Array<T>(length);
-//     }
-// }
+import {ZLinkedList} from '@/struct/ZLinkedList';
+export class ZSetUnion<T> {
+    private DEFAULT_MAX_LENGTH = 100;
+    private set:Array<ZLinkedList<T>>;
+
+    constructor(length: number) {
+      length ||= this.DEFAULT_MAX_LENGTH;
+      this.set = new Array<ZLinkedList<T>>(length).fill(new ZLinkedList<T>());
+    }
+
+    addSet() {
+      this.set.push(new ZLinkedList<T>());
+    }
+
+    delSet(index:number) {
+      this.set.splice(index, 1);
+    }
+}
