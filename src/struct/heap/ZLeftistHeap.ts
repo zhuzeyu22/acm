c4ba2c4d99704519704cc6bede6dcf65869d0f77
zhuzyu <zzyy1025>
2021-04-09 17:22:02 +0800

    save

--- src/struct/heap/ZLeftistHeap.ts
+++ src/struct/heap/ZLeftistHeap.ts
@@ -1,15 +1,19 @@
 import {swap} from '@/utils/swap';
 
+// 大根堆举例
 export class ZLeftistHeap {
     // dist 距离，节点到最近叶子节点的距离，叶子节点的 dist 为 0
     dist: number;
+    top: ZLeftistHeap;
     father: ZLeftistHeap;
     left: ZLeftistHeap;
     right: ZLeftistHeap;
-    value:number;
+    value: number;
+
 
     constructor() {
       this.dist = 0;
+      this.top = this;
       this.father = null;
       this.left = null;
       this.right = null;
@@ -19,8 +23,46 @@ export class ZLeftistHeap {
       if (!x || !y) {
         return x || y;
       }
+
       if (x.value < y.value) {
         [x, y] = swap(x, y);
+      }
+
+      x.right = ZLeftistHeap.merge(x.right, y);
+      x.right.father = x;
+
+      if (x.right.dist > x.left.dist) {
+        [x.left, x.right] = swap(x.left, x.right);
+      }
+
+      x.dist = x.right.dist + 1;
+      return x;
+    }
+
+    insert(x: ZLeftistHeap) {
+      return ZLeftistHeap.merge(this, x);
+    }
+
+    findTop() {
+      while (this.top !== this) {
+
+      }
+    }
+
+    public static update(x: ZLeftistHeap) {
+      if (!x) {
+        return;
+      }
+      if (x.dist !== x.right.dist + 1) {
+        x.dist = x.right.dist + 1;
+        ZLeftistHeap.update(x.father);
+      }
+    }
+
+    delete(x: ZLeftistHeap) {
+      ZLeftistHeap.merge(x.left, x.left);
+      if (x.father) {
+        x.father;
       }
     }
 }
